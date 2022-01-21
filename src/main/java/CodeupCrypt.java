33894cf9e287bdb14dc9bd8065e5c888fd0fc3dc
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-21 10:42:19 -0600

    codeup crypt

--- src/main/java/CodeupCrypt.java
+++ src/main/java/CodeupCrypt.java
@@ -1,2 +1,12 @@
 public class CodeupCrypt {
+    public static double version = 0.0;
+
+
+    public static String hashPassword(String password){
+        return "";
+    }
+
+    public static boolean checkPassword(String password, String hash){
+        return true;
+    }
 }

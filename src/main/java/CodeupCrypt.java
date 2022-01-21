5f730a3263eafb3ff1a6677c94651cbeea75ccd3
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-21 11:17:25 -0600

    junit testing lectures

--- src/main/java/CodeupCrypt.java
+++ src/main/java/CodeupCrypt.java
@@ -3,10 +3,36 @@ public class CodeupCrypt {
 
 
     public static String hashPassword(String password){
-        return "";
+        String hash = "";
+        for (char character: password.toCharArray()){
+            switch (character){
+                case 'a':
+                case 'A':
+                    hash += 4;
+                    break;
+                case 'e':
+                case 'E':
+                    hash += 3;
+                    break;
+                case 'i':
+                case 'I':
+                    hash += 1;
+                    break;
+                case 'o':
+                case 'O':
+                    hash += 0;
+                case 'u':
+                case 'U':
+                    hash += 9;
+                    break;
+
+            }
+        }
+
+        return hash;
     }
 
     public static boolean checkPassword(String password, String hash){
-        return true;
+        return hash.equals(hashPassword(password));
     }
 }

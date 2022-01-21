636b972861999de73115ada98a16c5413acd9677
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-21 15:59:33 -0600

    working on exercise 2

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -1,3 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class CohortTest {
+    Cohort empty;
+    Cohort one;
+    Cohort many;
+
+    @Before
+    public void init(){
+        empty = new Cohort();
+        one = new Cohort();
+        many = new Cohort();
+
+        Student ntoi = new Student(15L, "Ntoi");
+        Student wanneh = new Student(5L, "Wanneh");
+
+        ntoi.addGrade(80);
+        ntoi.addGrade(100);
+    }
+
+    @Test
+    public void addStudent(){
+    assertEquals(1,);
+    }
 
 }

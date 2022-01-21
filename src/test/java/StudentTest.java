e8ac285af8f9b1c8e4bb553672ddfa2e127d1993
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-01-21 12:18:03 -0600

    junit exercise

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class StudentTest {
+
+    @Test
+    public void getId(){
+        this.getId("Long");
+    }
+
+    private void getId(String aLong) {
+
+    }
+
+}
